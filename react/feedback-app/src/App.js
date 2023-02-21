38211cef204882fd190af27045eea004e01b4a81
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-02-21 20:08:30 +0100

    Simple examples how to use dynamic variables in React

--- react/feedback-app/src/App.js
+++ react/feedback-app/src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const title = "The newest of new";
+  const paragraph = "Checkout all the latest stuff";
+  const comments = [
+    { id: 1, text: "Comment One" },
+    { id: 2, text: "Comment Two" },
+    { id: 3, text: "Comment Three" },
+    { id: 4, text: "Comment Four" },
+    { id: 5, text: "Comment Five" },
+    { id: 6, text: "Comment Six" },
+  ];
+
+  return (
+    <>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{paragraph}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
